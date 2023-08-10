44e785bc1db69f54d68bca8a62e1b95676af7ed7
sora2919 <sora2919@gmail.com>
2023-08-10 19:27:05 +0800

    聊天室-聊天對象篩選ing

---
+++ slnWantSoraCoreMVC/WantSoraCoreMVC/Controllers/ChatApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WantSoraCoreMVC.Models;
+
+namespace WantSoraCoreMVC.Controllers
+{
+    public class ChatApiController : Controller
+    {
+
+        int loginID = 56;//先綁死ID登入
+        NewIspanProjectContext db = new NewIspanProjectContext();
+
+
+        public IActionResult UserList()
+        {
+            var orderMessages = db.ChatMessages
+                                .OrderByDescending(m => m.Created) // 按照時間戳排序，最新的在前面
+                                .ToList();
+
+            var users= orderMessages
+                      .Where(m=>m.SenderId== loginID||m.ReceiverId==loginID)
+                      .OrderByDescending(m => m.Created)
+                      .Select(m=>m.SenderId==loginID?m.ReceiverId:m.SenderId)
+                      .Distinct().ToList();
+
+            //JOIN參考資料 https://ithelp.ithome.com.tw/articles/10196394?sc=iThelpR
+            var usersInfo = users
+                .Join(db.MemberAccounts, a => a, member => member.AccountId, (userId, member) => member)
+                .Select(u => new
+                {
+                    u.Name,
+                    u.UserName,
+                    u.MemberPhoto
+                })
+                .ToList();
+
+            return Json(usersInfo);
+        }
+    }
+}
--- slnWantSoraCoreMVC/WantSoraCoreMVC/Controllers/ChatController.cs
+++ slnWantSoraCoreMVC/WantSoraCoreMVC/Controllers/ChatController.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WantSoraCoreMVC.Models;
 
 namespace WantSoraCoreMVC.Controllers
 {
     public class ChatController : Controller
     {
+        int loginID = 56;//先綁死ID登入
+        NewIspanProjectContext db = new NewIspanProjectContext();
+
         public IActionResult Index()
         {
             return View();
         }
+
     }
 }
